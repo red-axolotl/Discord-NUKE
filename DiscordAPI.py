356afa5fcd2cbb4504d0b9292e6f71f6ea0d1c6f
red-axolotl <red.axolotl@gmail.com>
2022-05-27 10:50:19 +0300

    Patched GetChannels, added RenameChannel

--- DiscordAPI.py
+++ DiscordAPI.py
@@ -1,4 +1,4 @@
-# import json
+import json
 import requests
 
 class Client:
@@ -11,5 +11,25 @@ class Client:
         req = requests.post(f"https://discord.com/api/v9/channels/{id}/messages", data=payload, headers=headers)
         return True if req.status_code == 200 else False
 
-    def RenameChannel(self) -> bool:
-        return
+    def GetChannels(self, guildID: int) -> dict:
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": self.token
+            }
+        req = requests.get(f"https://discord.com/api/v9/guilds/{guildID}/channels", headers=headers)
+        return req.json()
+
+    def RenameChannel(self, channelID: int, name: str) -> bool:
+        name.replace(' ', '-')
+        illegal = "`~!@#$%^&*()+={}[]:;\"\'|\\<,>.?/"
+        for char in illegal:
+            name.replace(char, '')
+
+        headers = {
+            "Authorization": self.token
+        }
+        data = {
+            "name": name
+        }
+        
+        return "retry_after" not in requests.patch(f"https://discord.com/api/v9/channels/{channelID}", json=data, headers=headers).json().keys()
